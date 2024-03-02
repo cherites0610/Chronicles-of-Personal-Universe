7376346cabc0e27f4409c6ba0cc7d16bd716e361
feather1217 <609sophie@gmail.com>
2024-03-02 22:11:22 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <div class="container py-4 px-3 mx-auto">
     <h1>Hello, Bootstrap and Vite!</h1>
     <button class="btn btn-primary">Primary button</button>
+    <h1>我是羽毛</h1>
   </div>
 </template>
 
