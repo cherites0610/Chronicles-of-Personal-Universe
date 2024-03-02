88e93ab1523e2494a1cc3e28c96dd71f4e70b233
cherites0610 <cherites0610@gmail.com>
2024-03-03 00:03:49 +0800

    commit

--- src/components/offCanvas.vue
+++ src/components/offCanvas.vue
@@ -6,22 +6,22 @@
 
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">我好懶得排這裡的版</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <ul class="nav flex-column">
+            <ul data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/#register">記錄本(測試註冊用)</a>
+                    <a class="nav-link" href="/#register">記錄本(測試註冊用)</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/#login">群組(測試登錄用)</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">筆記本</a>
+                    <a class="nav-link" href="#offcanvasExample">筆記本</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">給宇宙的話</a>
+                    <a class="nav-link" href="#offcanvasExample">給宇宙的話</a>
                 </li>
             </ul>
         </div>
