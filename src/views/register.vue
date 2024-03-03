85943c698fd2593dddf8c4057e65e88fdd3c2a4b
feather1217 <609sophie@gmail.com>
2024-03-04 01:04:38 +0800

    Ant Design裝好了

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         register
+        <a-button>Add</a-button>
+        
     </div>
 </template>
 
 <script setup>
-
+    
+    
 </script>
 
 <style lang="scss" scoped>
