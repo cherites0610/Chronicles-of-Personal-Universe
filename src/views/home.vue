20d381c389b2b7016990723d3ad24fc46e1f5540
feather1217 <609sophie@gmail.com>
2024-03-06 14:02:12 +0800

    增加事項->時間完成

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,9 +4,9 @@
         <a-calendar v-model:value="selectTime" @panelChange="onPanelChange" />
         <div>
             <TimingScheduleCard  :selectTime="selectTime.format('YYYY/MM/DD dddd')"/>
-            <a-button type="primary" @click="showModal">增加</a-button>
+            <a-float-button type="primary" @click="showModal" style="width:80px; height: 80px;"/>
             <div class="addDates">
-                <a-modal v-model:open="open" width="400px" title="增加事項" @ok="handleOk" style="text-align: center;">
+                <a-modal v-model:open="open" width="400px" title="增加事項"  style="text-align: center;">
                     <template #footer>
                         <a-flex justify="center">
                             <a-button key="submit" type="primary" style="background-color: #288CA3;" @click="handleOk">確定</a-button>
@@ -15,32 +15,31 @@
                     <a-input v-model:value="value" :bordered="false" placeholder="標題" style="font-size: large;"/>
 
                     
-
+                    <!--時間-->
                     <a-flex justify="flex-start" style="padding-top:10px ;"  >
-                        <a-divider type="vertical" style="height: 80px; background-color: #288CA3" />
+                        <a-divider type="vertical" style="height: 100px; background-color: #288CA3" />  
                         <FieldTimeOutlined/>
-                        <div style="padding-left: 10px; width: 15%;" >
+                        <div style="padding-left: 10px; width: 25%; text-align: left;"  >
                             全天 :<br/>
-                            <div style="margin-top: 15px;">
-                                日期 :
-                                
-                            </div>
+                            <div style="padding-top: 15px; height:10%;">開始日期 :</div><br/>
+                            <div style="padding-top: 15px; ;">結束日期 :</div>
                         </div>
-                        <div style="padding-left: 20px; width: 70%;"> 
+                        <div style="padding-left: 25px; width: 75%; margin-right: 5%;"> 
                             <a-flex justify="flex-end" vertical>
-                                <a-switch v-model:checked="disabled" style="margin-left: 80%; background-color: #288CA3;"/>
-                                <a-flex justify="flex-end">
-                                    <a-date-picker size="small" style="margin-top: 15px; max-width: 45%;" @change="onChange" @ok="onOk"></a-date-picker>
-                                    <a-time-picker style="margin-top: 15px; max-width:45% ;" size="small"></a-time-picker>
+                                <a-switch v-model:checked="checked" @click="showTime" style="margin-left: 80%;"/>
+                                <a-flex justify="flex-end"><!--開始日期-->
+                                    <a-date-picker size="small" style="margin-top: 15px; max-width: 55%;" @change="onChange" @ok="onOk" placeholder="date"></a-date-picker>                                   
+                                    <a-time-picker v-if="checked" style="margin-top: 15px; max-width:35% ; margin-left: 5px;" format="HH:mm" placeholder="time" size="small"></a-time-picker>
                                 </a-flex>
-                                
+                                <a-flex justify="flex-end"><!--結束日期-->
+                                    <a-date-picker size="small" style="margin-top: 15px; max-width: 55%;" @change="onChange" @ok="onOk" placeholder="date"></a-date-picker>                                   
+                                    <a-time-picker v-if="checked" style="margin-top: 15px; max-width:35% ; margin-left: 5px;" format="HH:mm" placeholder="time" size="small"></a-time-picker>
+                                </a-flex>
                             </a-flex>                          
-                                
                         </div>
-                        
                     </a-flex>
                     
-                    
+                    <!--提醒-->
                     
                     
                 </a-modal>
@@ -60,11 +59,15 @@ import { ref } from 'vue';
 import { FieldTimeOutlined } from '@ant-design/icons-vue';
 const selectTime = ref(dayjs());
 const onPanelChange = (value, mode) => {
-    console.log(value, mode);
+    //console.log(value, mode);
 };
 console.log(dayjs.locale())
 
-
+const timeTTMM=ref(false)
+const showTime=()=>{
+        if(checked.value==false){console.log("關"+checked.value);}
+        else{console.log("開"+checked.value);}
+}
 
 //以下是在做增加
 const open = ref(false);
@@ -72,17 +75,15 @@ const showModal = () => { //主介面點開
   open.value = true;
 };
 const handleOk = e => { //點開來後確定
-  console.log(e);
-  open.value = false;
-};
-const handleCancel = () => { //點開來後取消
+  //console.log(e);
   open.value = false;
 };
 
 //讓全天鈕可以動(disabled=true時會鎖定)
-const disabled = ref(false);
+const checked = ref(false);
 
-const showTime=ref("");
+
+
 </script>
 
 <style lang="scss" scoped>
