ec2e9ffa060e67657052d398df535d5e0f9f036f
feather1217 <609sophie@gmail.com>
2024-03-13 14:29:34 +0800

    群組增加功能卡片完成

--- src/views/group.vue
+++ src/views/group.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
         <addGroup/>
+        <a-float-button type="primary" @click="showModal" style="width:80px; height: 80px;"/>
+        <div>
+            <a-modal v-model:open="open" width="400px" title="增加群組"  style="text-align: center;">
+                <template #footer>
+                        <a-flex justify="center">
+                            <a-button key="submit" type="primary" style="background-color: #288CA3;" @click="handleOk">確定</a-button>
+                        </a-flex>
+                </template>
+                
+            </a-modal>
+        </div>
+        
     </div>
 </template>
 
 <script setup>
-    import addGroup from '../components/addGroup.vue';
+import addGroup from '../components/addGroup.vue';
+import {ref} from 'vue';
+const open = ref(false);
+const showModal = () => { //主介面點開
+    console.log('click');
+    open.value = true;
+};
+
 </script>
 
 <style lang="scss" scoped>
