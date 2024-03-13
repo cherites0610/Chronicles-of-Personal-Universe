a219802af75348f48c8b053b06aea301d843594a
feather1217 <609sophie@gmail.com>
2024-03-13 18:00:56 +0800

    增加跟加入群組浮動按鈕完成

--- src/views/group.vue
+++ src/views/group.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
         <addGroup/>
-        <a-float-button type="primary" @click="showModal" style="width:80px; height: 80px;"/>
+        <a-float-button type="primary" @click="addNewGroup" style="width:80px; height: 80px;"/>
         <div>
             <a-modal v-model:open="open" width="400px" title="增加群組"  style="text-align: center;">
                 <template #footer>
-                        <a-flex justify="center">
-                            <a-button key="submit" type="primary" style="background-color: #288CA3;" @click="handleOk">確定</a-button>
-                        </a-flex>
+                    <a-flex justify="center">
+                        <a-button key="submit" type="primary" style="background-color: #288CA3;" @click="handleOk">確定</a-button>
+                    </a-flex>
                 </template>
-                
+                hi
+            </a-modal>
+        </div>
+        
+        <a-float-button type="defult" @click="joinGroup" style="width:80px; height: 80px; right: 120px;"/>
+        <div>
+            <a-modal v-model:open="joing" width="400px" title="新增群組"  style="text-align: center;">
+                <template #footer>
+                    <a-flex justify="center">
+                        <a-button key="submit" type="primary" style="background-color: #288CA3;" @click="handleOk">確定</a-button>
+                    </a-flex>
+                </template>
+                yo
             </a-modal>
         </div>
         
@@ -19,12 +31,20 @@
 <script setup>
 import addGroup from '../components/addGroup.vue';
 import {ref} from 'vue';
+
+//增加群組
 const open = ref(false);
-const showModal = () => { //主介面點開
+const addNewGroup = () => { 
     console.log('click');
     open.value = true;
 };
 
+//加入群組
+const joing=ref(false);
+const joinGroup=()=>{
+    joing.value=true;
+    console.log('c')
+}
 </script>
 
 <style lang="scss" scoped>
