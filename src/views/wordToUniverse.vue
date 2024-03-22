6ca6e639350caa924fd5ed7055d2651b2d28e508
cherites0610 <cherites0610@gmail.com>
2024-03-22 12:01:38 +0800

    給宇宙的話適配完畢

--- src/views/wordToUniverse.vue
+++ src/views/wordToUniverse.vue
@@ -2,31 +2,34 @@
     <span id="title">給宇宙的話</span>
 
     <a-spin :spinning="spinning" size="large">
-        <contentCard v-for="(key, index) in comments">
+        <contentCard @clickCard="handleCardClick(index)" v-for="(key, index) in comments">
             <h3>
                 {{ key.year }}
             </h3>
-            <a-typography-paragraph :ellipsis="{rows:4}" :content="key.comment" />
+            <a-typography-paragraph :ellipsis="{ rows: 4 }" :content="key.comment" />
         </contentCard>
     </a-spin>
 
-    <a-float-button type="primary" @click="formState.isNew = true, showModel = true"
+    <a-float-button type="primary" @click="formState.isNew = true, showCreatModel = true" v-if="true"
         style="position: absolute;width:80px; height: 80px;" />
-    <a-modal :footer="null" :afterClose="modalCancel" :title="formState.year.year()" v-model:open="showModel">
-        <div v-if="!formState.isNew">
-            <a-typography-paragraph stlye="width=100%" v-model:content="formState.comment"
-                :editable="dayjs().year() == formState.year.year()" />
-        </div>
-
-
-        <a-form v-if="formState.isNew" @finish="onFinish" :model="formState">
+    <a-modal :footer="null" :afterClose="modalCancel" title="留言" v-model:open="showCreatModel">
+        <a-form v-if="formState.isNew" :model="formState">
             <a-form-item :rules="[{ required: true, message: '請選擇月份' }]" name="year" label="年份">
                 <a-date-picker disabled v-model:value="formState.year" picker="year" />
             </a-form-item>
             <a-form-item name="comment" :rules="[{ required: true, message: '一定要留言' }]" label="說話">
                 <a-textarea v-model:value="formState.comment" style="height: 200px;" />
             </a-form-item>
+            <a-form-item>
+                <a-flex justify="center">
+                    <a-button key="submit" type="primary" style="background-color: #288CA3;">確定</a-button>
+                </a-flex>
+            </a-form-item>
         </a-form>
+    </a-modal>
+
+    <a-modal :title="editState.year" @handleOk="showEditModel=false" v-model:open="showEditModel">
+        <a-typography-paragraph v-model:content="editState.comment" :editable="editState.year!=Number(dayjs().format('YYYY'))" />
     </a-modal>
 
     <div>
@@ -40,22 +43,19 @@ import dayjs from 'dayjs';
 import contentCard from '../components/contentCard.vue'
 import { getWordToUById } from '../api/scheduleApi'
 
-const onExpand = (event) => {
-    console.log(event)
-}
-
-const onEllipsis = (event) => {
-    console.log(evenet)
-}
-
 const comments = ref([]);
 const isEmpty = ref(false);
-const showModel = ref(false)
+const showCreatModel = ref(false)
+const showEditModel = ref(false)
 const spinning = ref(true);
 const formState = ref({
-    isNew: true,
     year: dayjs(),
     comment: ''
+})
+
+const editState = ref({
+    year:2023 ,
+    comment: '',
 })
 
 getWordToUById('1').then((result) => {
@@ -63,17 +63,12 @@ getWordToUById('1').then((result) => {
     spinning.value = false
 })
 
-const onFinish = values => {
-    console.log('Success:', values);
-    formState.value.comment = ''
-    showModel.value = false
-};
+const handleCardClick = (index) => {
+    showEditModel.value = true;
+    editState.value.year=comments.value[index].year;
+    editState.value.comment=comments.value[index].comment
+    console.log(Number(dayjs().format('YYYY')))
 
-const handleClick = (index) => {
-    showModel.value = true;
-    formState.value.isNew = false
-    formState.value.comment = comments[index].comment
-    formState.value.year = dayjs().year(comments[index].year)
 }
 
 const modalCancel = () => {
