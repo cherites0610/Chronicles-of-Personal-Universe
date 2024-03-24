fa707fc5a436fd9cbe61c250630b9552122f1b4e
feather1217 <609sophie@gmail.com>
2024-03-24 22:14:17 +0800

    做了大哥會頭痛的雛型

---
+++ src/components/checkListBox.vue
@@ -0,0 +1,49 @@
+<template>
+    <div style="">
+    <a-row>
+        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
+            <a-card size="large" title="⚝ 重要且緊急" style="background-color: #FFDCDC;">
+                <p>card content</p>
+                <p>card content</p>
+                <p>card content</p>
+            </a-card>                
+        </a-col>
+        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
+            <a-card size="large" title="⚝ 重要不緊急" style=" background-color: #E7FBDB;">
+                <p>card content</p>
+                <p>card content</p>
+                <p>card content</p>
+            </a-card>        
+        </a-col>
+        
+    </a-row>
+    
+    <a-row>
+        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
+            <a-card size="large" title="⚝ 不重要但緊急" style=" background-color: #D9E4FF;">
+                <p>card content</p>
+                <p>card content</p>
+                <p>card content</p>
+            </a-card>                
+        </a-col>
+        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
+            <a-card size="large" title="⚝ 不重要不緊急" style=" background-color: #FFFFFF;">
+                <p>card content</p>
+                <p>card content</p>
+                <p>card content</p>
+            </a-card>        
+        </a-col>        
+    </a-row>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+#card{
+    height: 100px;
+    background-color: rgb(105, 202, 202);
+}
+</style>
--- src/views/checkList.vue
+++ src/views/checkList.vue
@@ -2,10 +2,12 @@
     <div>
         記錄本
     </div>
+    <checkListBox/>
+    
 </template>
 
 <script setup>
-
+    import checkListBox from "../components/checkListBox.vue" ;
 </script>
 
 <style lang="scss" scoped>
