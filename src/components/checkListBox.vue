11255b01ad0ea2e918206f360bc2c1c6b111b653
feather1217 <609sophie@gmail.com>
2024-03-24 23:35:26 +0800

    讓大哥頭痛2.0 (資料部分)

--- src/components/checkListBox.vue
+++ src/components/checkListBox.vue
@@ -2,17 +2,19 @@
     <div style="">
     <a-row>
         <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
-            <a-card size="large" title="⚝ 重要且緊急" style="background-color: #FFDCDC;">
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
+            <a-card hoverable size="large" title="⚝ 重要且緊急"  style="background-color: #FFDCDC;">
+                <li v-for="task in imUrgent" :key="task.commit">
+                    <a-radio></a-radio>
+                    {{ task.commit }}
+                </li>
             </a-card>                
         </a-col>
         <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
             <a-card size="large" title="⚝ 重要不緊急" style=" background-color: #E7FBDB;">
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
+                <li v-for="task in imNourgent" :key="task.commit">
+                    <a-radio></a-radio>
+                    {{ task.commit }}
+                </li>
             </a-card>        
         </a-col>
         
@@ -21,16 +23,18 @@
     <a-row>
         <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
             <a-card size="large" title="⚝ 不重要但緊急" style=" background-color: #D9E4FF;">
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
+                <li v-for="task in unimUrgent" :key="task.commit">
+                    <a-radio></a-radio>
+                    {{ task.commit }}
+                </li>
             </a-card>                
         </a-col>
         <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
             <a-card size="large" title="⚝ 不重要不緊急" style=" background-color: #FFFFFF;">
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
+                <li v-for="task in unimNourgent" :key="task.commit">
+                    <a-radio></a-radio>
+                    {{ task.commit }}
+                </li>
             </a-card>        
         </a-col>        
     </a-row>
@@ -39,6 +43,23 @@
 
 <script setup>
 
+    const imUrgent =[ //重要且緊急
+        { commit: "期中考" },
+        { commit: "超市特價" },
+        { commit: "開會"},
+    ]
+    const unimUrgent =[ //不重要緊急
+        { commit: "上廁所" },
+        { commit: "洗衣服" },
+    ]
+    const imNourgent =[ //重要不緊急
+        { commit: "英文免修" },
+        { commit: "期末報告" },
+    ]
+    const unimNourgent =[ //不重要不緊急
+        { commit: "玩遊戲" },
+        { commit: "大哥" },
+    ]
 </script>
 
 <style lang="scss" scoped>
