dbec317a5c29bda3218faf1c88fb45c5a12a41f9
cherites0610 <cherites0610@gmail.com>
2024-03-27 18:55:09 +0800

    共用modal調整

--- src/components/checkListBox.vue
+++ src/components/checkListBox.vue
@@ -1,144 +1,114 @@
 <template>
     <div class="container-center">
-    <a-row>
-        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
-            <a-card hoverable size="large" @click="cardClick('1')"  title="⚝ 重要且緊急"  style="background-color: #FFDCDC;">
-                <li v-for="task in imUrgent" :key="task.commit">
-                    <div >
+        <a-row>
+            <a-col :span="10" style="padding-top:30px; padding-left: 30px;">
+                <a-card hoverable size="large" @click="cardClick('1')" title="⚝ 重要且緊急"
+                    style="background-color: #FFDCDC;">
+                    <li v-for="task in imUrgent" :key="task.commit">
+                        <div>
+                            <a-radio style="z-index: 1000;" @click="handleRadioClick(task.commit)">
+                                {{ task.commit }}
+                            </a-radio>
+                        </div>
+                    </li>
+                </a-card>
+            </a-col>
+            <a-col :span="10" style="padding-top:30px; padding-left: 30px;">
+                <a-card hoverable size="large" @click="cardClick('2')" title="⚝ 重要不緊急"
+                    style=" background-color: #E7FBDB;">
+                    <li v-for="task in imNourgent" :key="task.commit">
                         <a-radio @click="handleRadioClick(task.commit)">
                             {{ task.commit }}
-                        </a-radio>                   
-                        
-                    </div>                                   
-                </li>
-            </a-card>                
-        </a-col>
-        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
-            <a-card hoverable size="large" @click="cardClick('2')" title="⚝ 重要不緊急" style=" background-color: #E7FBDB;">
-                <li v-for="task in imNourgent" :key="task.commit">
-                    <a-radio @click="handleRadioClick(task.commit)">
+                        </a-radio>
+                    </li>
+                </a-card>
+            </a-col>
+        </a-row>
+        <a-row>
+            <a-col :span="10" style="padding-top:30px; padding-left: 30px;">
+                <a-card hoverable size="large" @click="cardClick('3')" title="⚝ 不重要但緊急"
+                    style=" background-color: #D9E4FF;">
+                    <li v-for="task in unimUrgent" :key="task.commit">
+                        <a-radio @click="handleRadioClick(task.commit)">
                             {{ task.commit }}
-                    </a-radio>
-                </li>
-            </a-card>        
-        </a-col>
-    </a-row>
-    <a-row>
-        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">   
-            <a-card hoverable size="large" @click="cardClick('3')" title="⚝ 不重要但緊急" style=" background-color: #D9E4FF;">
-                <li v-for="task in unimUrgent" :key="task.commit">
-                    <a-radio @click="handleRadioClick(task.commit)">
+                        </a-radio>
+                    </li>
+                </a-card>
+            </a-col>
+            <a-col :span="10" style="padding-top:30px; padding-left: 30px;">
+                <a-card hoverable size="large" @click="cardClick('4')" title="⚝ 不重要不緊急"
+                    style=" background-color: #FFFFFF;">
+                    <li v-for="task in unimNourgent" :key="task.commit">
+                        <a-radio @click="handleRadioClick(task.commit)">
                             {{ task.commit }}
-                    </a-radio>
-                </li>
-            </a-card>                
-        </a-col>
-        <a-col :span="10" style="padding-top:30px; padding-left: 30px;">           
-            <a-card hoverable size="large" @click="cardClick('4')" title="⚝ 不重要不緊急" style=" background-color: #FFFFFF;">
-                <li v-for="task in unimNourgent" :key="task.commit">
-                    <a-radio @click="handleRadioClick(task.commit)">
-                            {{ task.commit }}
-                    </a-radio>
-                </li>
-            </a-card>        
-        </a-col>        
-    </a-row>
+                        </a-radio>
+                    </li>
+                </a-card>
+            </a-col>
+        </a-row>
     </div>
 
-    <!-- 以下在做點開來顯示細項 -->   
-    <a-modal forceRender="true" v-model:open="a" :footer="null" style="width: 40%;">           
+    <!-- 以下在做點開來顯示細項 -->
+    <a-modal :forceRender="true" v-model:open="showModel" :footer="null" style="width: 40%;">
         <div style="text-align: center;">
             <a-typography-title :level="4">⚝ 重要且緊急</a-typography-title>
         </div>
         <li v-for="task in imUrgent" :key="task.commit" style="padding-top: 5px;">
             <a-radio>
                 {{ task.commit }}
-                </a-radio>                       
-        </li>           
+            </a-radio>
+        </li>
     </a-modal>
-    <a-modal forceRender="true" v-model:open="b" :footer="null" style="width: 40%;">           
-        <div style="text-align: center;">
-            <a-typography-title :level="4">⚝ 重要不緊急</a-typography-title>
-        </div>
-        <li v-for="task in imNourgent" :key="task.commit" style="padding-top: 5px;">
-            <a-radio>
-                {{ task.commit }}
-                </a-radio>                       
-        </li>           
-    </a-modal>
-    <a-modal forceRender="true" v-model:open="c" :footer="null" style="width: 40%;">           
-        <div style="text-align: center;">
-            <a-typography-title :level="4">⚝ 不重要但緊急</a-typography-title>
-        </div>
-        <li v-for="task in unimUrgent" :key="task.commit" style="padding-top: 5px;">
-            <a-radio>
-                {{ task.commit }}
-                </a-radio>                       
-        </li>           
-    </a-modal>
-    <a-modal forceRender="true" v-model:open="d" :footer="null" style="width: 40%;">           
-        <div style="text-align: center;">
-            <a-typography-title :level="4">⚝ 不重要不緊急</a-typography-title>
-        </div>
-        <li v-for="task in unimNourgent" :key="task.commit" style="padding-top: 5px;">
-            <a-radio>
-                {{ task.commit }}
-                </a-radio>                       
-        </li>           
-    </a-modal>
-    
+
 </template>
 
 <script setup>
-    import {ref} from 'vue' ;
-    const imUrgent =[ //重要且緊急
-        { commit: "期中考" },
-        { commit: "超市特價" },
-    ]
-    const unimUrgent =[ //不重要緊急
-        { commit: "上廁所" },
-        { commit: "洗衣服" },
-    ]
-    const imNourgent =[ //重要不緊急
-        { commit: "英文免修" },
-        { commit: "期末報告" },
-    ]
-    const unimNourgent =[ //不重要不緊急
-        { commit: "玩遊戲" },
-        { commit: "大哥" },
-    ]
+import { ref } from 'vue';
+const imUrgent = [ //重要且緊急
+    { commit: "期中考" },
+    { commit: "超市特價" },
+]
+const unimUrgent = [ //不重要緊急
+    { commit: "上廁所" },
+    { commit: "洗衣服" },
+]
+const imNourgent = [ //重要不緊急
+    { commit: "英文免修" },
+    { commit: "期末報告" },
+]
+const unimNourgent = [ //不重要不緊急
+    { commit: "玩遊戲" },
+    { commit: "大哥" },
+]
 
-    //以下在做對話框
-    const showModel = ref(false);
-    const a = ref(false);
-    const b = ref(false);
-    const c = ref(false);
-    const d = ref(false);
-    const cardClick = (title) => {
-        console.log("為"+title);
-        if(title==1){a.value = true ;}
-        if(title==2){b.value = true ;}
-        if(title==3){c.value = true ;}
-        if(title==4){d.value = true ;}
+//以下在做對話框
+const showModel = ref(false);
+
+const cardClick = (index) => {
+    const item = document.getElementsByClassName('ant-modal-content');
+    switch(Number(index)){
+        case 1:
+            console.log(123)
+            item[0].style.backgroundColor = "#FFDCDC"
+            break;
+        case 2:
+            item[0].style.backgroundColor = "#E7FBDB"
+            break;
+        case 3:
+            item[0].style.backgroundColor = "#D9E4FF"
+            break;
+        case 4:
+            item[0].style.backgroundColor = "#FFFFFF"
+            break;
     }
-   
+    showModel.value = true
+}
 
-    onMounted(()=> {
-        const item = document.getElementsByClassName('ant-modal-content');
-        item[0].style.backgroundColor="#FFDCDC";
-        item[1].style.backgroundColor="#E7FBDB";
-        item[2].style.backgroundColor="#D9E4FF";
-        item[3].style.backgroundColor="#FFFFFF";
-        
-    })
-
-    //以下在做radio點擊事件
-    const handleRadioClick = (taskCommit) => {
-        console.log('Task commit:', taskCommit);
-    };
+//以下在做radio點擊事件
+const handleRadioClick = (taskCommit) => {
+    console.log('Task commit:', taskCommit);
+};
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
